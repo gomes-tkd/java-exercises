6bf0a35e4013f9480377fd82fece932e645fd21d
gomes-tkd <jgomestkd@gmail.com>
2024-09-08 23:23:24 -0300

    Exercício 2: Casos 1 e 2 concluídos

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.CANADA);
         Scanner sc = new Scanner(System.in);
-
+        
+        // atividade1(sc);
+        caso1();
         caso2();
-
         sc.close();
 
     }
@@ -77,20 +78,20 @@ public class Main {
 
     public static void atividade2(Scanner sc) {}
 
-    // não consegui imprimir invertido
+    // Impressão done
     public static void caso1() {
-        for (int i = 4; i >= 0; i--) {
-            for (int j = 5 - i; j >= 1; j--) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
     }
 
-    // to no caminho certo - acredito
+    // impressão done
     public static void caso2() {
-        for (int i = 4; i >= 0; i--) {
-            for (int j = 5 - i; j >= 1; j--) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(i);
             }
             System.out.println();
